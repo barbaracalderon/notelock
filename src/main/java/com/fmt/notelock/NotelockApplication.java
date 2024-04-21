3c5a46401c9832aa40ead1cbf3e9eaf3c7bbde57
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-20 21:43:12 -0300

    first commit

---
+++ src/main/java/com/fmt/notelock/NotelockApplication.java
@@ -0,0 +1,13 @@
+package com.fmt.notelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotelockApplication.class, args);
+	}
+
+}
